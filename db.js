322f0c3c35520bad6e32f50696d03a6649b486fd
ric <ricard.more@weareadaptive.com>
2022-02-08 14:41:25 +0100

    Setup connection to Heroku DB

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
 const {Pool} = require("pg");
 const config = require('config');
 
-const pool = new Pool({
+let poolConfig = {
     user: config.get('database.user'),
     host: config.get('database.host'),
     database: config.get('database.dbname'),
     password: config.get('database.password'),
     port: config.get('database.port')
-});
+}
+
+let enableRejectUnauthorized = config.get('database.enableRejectUnauthorized')
+
+if (enableRejectUnauthorized) {
+    poolConfig.ssl = {rejectUnauthorized: false}
+}
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
